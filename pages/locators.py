eb17e5025b787b47fd34a66e8fcb5f2c29307527
b1orhythm <ilya.berdnikov21@gmail.com>
2023-07-27 10:00:01 +0300

    Выполнение задания "4.3 Улучшаем дизайн тестов (шаг 2)" (https://stepik.org/lesson/201964/step/2?unit=176022)

--- pages/locators.py
+++ pages/locators.py
@@ -8,3 +8,12 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+
+class ProductPageLocators():
+    PRODUCT_TITLE = (By.XPATH, "//div[contains(@class, 'product_main')]/h1")
+    PRODUCT_PRICE = (By.XPATH, "//div[contains(@class, 'product_main')]/p[@class='price_color']")
+    BASKET_PRICE = (By.XPATH, "//div[contains(@class, 'basket-mini')]")
+    ADD_TO_BASKET_BTN = (By.CSS_SELECTOR, "#add_to_basket_form")
+    MESSAGE_WITH_PRODUCT_TITLE = (By.XPATH, "//div[contains(@class, 'alert-success')]/div[@class='alertinner ']/strong")
+    MESSAGE_WITH_PRICE_BASKET = (By.XPATH, "//div[contains(@class, 'alert-info')]/div[@class='alertinner ']/p/strong")
---
+++ pages/product_page.py
@@ -0,0 +1,23 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+
+class ProductPage(BasePage):
+    def add_product_to_basket(self):
+        self.should_be_add_to_basket_btn()
+        self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BTN).click()
+        self.solve_quiz_and_get_code()
+        self.should_be_message_about_added_product()
+        self.should_be_same_price_in_basket()
+
+
+    def should_be_add_to_basket_btn(self):
+        assert self.is_element_present(*ProductPageLocators.ADD_TO_BASKET_BTN), "\"Add to basket\" button is not presented"
+
+    def should_be_message_about_added_product(self):
+        product_title = self.browser.find_element(*ProductPageLocators.PRODUCT_TITLE).text
+        assert product_title in self.browser.find_element(*ProductPageLocators.MESSAGE_WITH_PRODUCT_TITLE).text, "The name of the added product is different from the product name on the page"
+
+    def should_be_same_price_in_basket(self):
+        product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
+        assert product_price == self.browser.find_element(*ProductPageLocators.MESSAGE_WITH_PRICE_BASKET).text, "The price of the cart does not match the price of the product"
