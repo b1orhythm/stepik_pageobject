7cd7989ecbeee968b0207a0684eaa3765244b858
b1orhythm <ilya.berdnikov21@gmail.com>
2023-07-28 11:17:19 +0300

    Выполнение задания "4.3 Улучшаем дизайн тестов (шаг 8)" (https://stepik.org/lesson/201964/step/8?unit=176022)

--- pages/base_page.py
+++ pages/base_page.py
@@ -2,13 +2,21 @@ from selenium.common.exceptions import NoAlertPresentException, NoSuchElementExc
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import math
-
+from .locators import BasePageLocators
 
 class BasePage():
     def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
         self.browser.implicitly_wait(timeout)
+
+    def go_to_login_page(self):
+        login_link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        login_link.click()
+        # return LoginPage(browser=self.browser, url=self.browser.current_url)
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
 
     def open(self):
         self.browser.get(self.url)
